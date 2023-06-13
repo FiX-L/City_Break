354a69c8aed23b0e520395f8db8dc1ceddb48ce5
Simon Chretien <simon.chretien62@gmail.com>
2023-06-13 15:14:14 +0200

    Countdown available

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -6,14 +6,12 @@ class UserGamesController < ApplicationController
 
 
   def create
-    raise
     @user_game = UserGame.new
     @user_game.user = current_user
     @user_game.game = Game.find(params[:game_id])
     @user_game.end_of_game_time = Time.now + @user_game.game.max_duration_in_sec
-    @user_game.end_of_game_time = @user_game.end_of_game_time + 3600 + 3600
     @user_game.save
-    redirect_to user_game_path(@user_game)
+    redirect_to enigma_path(@user_game.game.enigmas.first)
   end
 
   def show
@@ -24,5 +22,13 @@ class UserGamesController < ApplicationController
   def progression
     @progression = Enigma.all
     @start = @progression.first
+  end
+
+  def penalty
+    @user_game = UserGame.find(params[:user_game_id])
+    @user_game.penalty += 1
+    @user_game.save
+    @enigma = Enigma.find(params[:enigma_id])
+    redirect_to enigma_path(@enigma)
   end
 end
